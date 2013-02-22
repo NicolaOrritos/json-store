3157ed6ab21979766df7fdc9e83a51469c067e41
Nicola Orritos <nicola.orritos@gmail.com>
2013-02-22 10:54:16 +0100

    Added more layout and minor features to the admin interface

--- src/store/routes/default.js
+++ src/store/routes/default.js
@@ -3,7 +3,60 @@
  * GET home page.
  */
 
+var redis = require("redis");
+
+var client = redis.createClient();
+
+client.on("error", function (err)
+{
+    console.log("Got an error from the Redis client: " + err);
+});
+
+
 exports.index = function(req, res)
 {
-    res.render('index', { pagename: 'Summary of keys' });
+    client.dbsize(function(err, keys_count)
+    {
+        client.keys("*", function(err, keys)
+        {
+            var keys_arr = undefined;
+        
+            if (err)
+            {
+                keys_arr = "[error retrieving keys from the DB]";
+            }
+            else
+            {
+                keys_arr = keys.toString().split(",");
+            }
+            
+            client.keys("tags*", function(err, tags_str)
+            {
+                var tags_count_text = "";
+                var tags_arr = undefined;
+            
+                if (err)
+                {
+                    tags_count_text = "[error retrieving tags from the DB]"
+                    tags_arr = "[error retrieving tags from the DB]";
+                }
+                else
+                {
+                    tags_arr = tags_str.toString().split(",");
+                    
+                    for (a=0; a<tags_arr.length; a++)
+                    {
+                        tags_arr[a] = tags_arr[a].substr(5);
+                    }
+                }
+                
+                res.render('index', {
+                    keys_count: keys_count,
+                    tags_count: tags_arr.length,
+                    keys_arr: keys_arr,
+                    tags_arr: tags_arr
+                });
+            });
+        });
+    });
 };
