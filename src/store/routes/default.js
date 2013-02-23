ca26cc8aaf68edcada2ad36a2912fb9889ed673d
Nicola Orritos <nicola.orritos@gmail.com>
2013-02-23 10:46:55 +0100

    Reworked the admin backend code and added IDs to the elements displayed

--- src/store/routes/default.js
+++ src/store/routes/default.js
@@ -32,56 +32,43 @@ if (typeof String.prototype.endsWith != 'function')
 
 exports.index = function(req, res)
 {
-    client.dbsize(function(err, keys_count)
+    client.keys("*", function(err, keys)
     {
-        client.keys("*", function(err, keys)
+        var keys_arr_filtered = undefined;
+    
+        if (keys)
         {
-            var keys_arr = undefined;
+            var keys_arr = keys.toString().split(",");
+            keys_arr_filtered = new Array();
+            
+            for (a=b=0; a<keys_arr.length; a++)
+            {
+                if (!keys_arr[a].startsWith("tags:"))
+                {
+                    keys_arr_filtered[b++] = keys_arr[a];
+                }
+            }
+        }
         
-            if (err)
+        client.keys("tags:*", function(err, tags_str)
+        {
+            var tags_arr = undefined;
+        
+            if (tags_str)
             {
-                keys_arr = "[error retrieving keys from the DB]";
-            }
-            else
-            {
-                keys_arr = keys.toString().split(",");
-                keys_arr_filtered = new Array();
+                tags_arr = tags_str.toString().split(",");
                 
-                for (a=b=0; a<keys_arr.length; a++)
+                for (a=0; a<tags_arr.length; a++)
                 {
-                    if (!keys_arr[a].startsWith("tags:"))
-                    {
-                        keys_arr_filtered[b++] = keys_arr[a];
-                    }
+                    tags_arr[a] = tags_arr[a].substr(5);
                 }
             }
             
-            client.keys("tags*", function(err, tags_str)
-            {
-                var tags_count_text = "";
-                var tags_arr = undefined;
-            
-                if (err)
-                {
-                    tags_count_text = "[error retrieving tags from the DB]"
-                    tags_arr = "[error retrieving tags from the DB]";
-                }
-                else
-                {
-                    tags_arr = tags_str.toString().split(",");
-                    
-                    for (a=0; a<tags_arr.length; a++)
-                    {
-                        tags_arr[a] = tags_arr[a].substr(5);
-                    }
-                }
-                
-                res.render('index', {
-                    keys_count: keys_count,
-                    tags_count: tags_arr.length,
-                    keys_arr: keys_arr_filtered,
-                    tags_arr: tags_arr
-                });
+            res.render('index', {
+                keys_count: keys_arr_filtered ? keys_arr_filtered.length : "[error retrieving keys from the DB]",
+                tags_count: tags_arr ? tags_arr.length : "[error retrieving tags from the DB]",
+                keys_arr: keys_arr_filtered,
+                tags_arr: tags_arr
             });
         });
     });
