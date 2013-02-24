5b38944c1f6bb27aa9b35b5de7d2aabef5fcfa6b
Nicola Orritos <nicola.orritos@gmail.com>
2013-02-24 13:27:54 +0100

    Towards completion of the search feature. Next step is populating the results view.

--- src/store/public/javascripts/functions.js
+++ src/store/public/javascripts/functions.js
@@ -2,6 +2,11 @@
 (function($){})(window.jQuery);
 
 
+String.prototype.killWhitespaces = function() {
+    return this.replace(/\s/g, '');
+};
+
+
 /* trigger when page is ready */
 $(document).ready(function ()
 {
@@ -36,12 +41,52 @@ $(document).ready(function ()
     {
         $("#content #docs-search #search-bytags").show("fast");
         $("#content #docs-search #search-byid").hide("fast");
+        
+        $("#docs-search .results").clean();
     });
     
     $("#docs-subnav #byid-search").click(function()
     {
         $("#content #docs-search #search-bytags").hide("fast");
         $("#content #docs-search #search-byid").show("fast");
+        
+        $("#docs-search .results").clean();
+    });
+    
+    $("#search-byid a").click(function()
+    {
+        var id = $("#search-byid input").val();
+        
+        console.log("id: " + id);
+        
+        $.getJSON('docs/' + id, function(data) {
+            
+            console.log("JSON returned:\n" + JSON.stringify(data));
+            
+            data.forEach(function(doc, position)
+            {
+                
+            });
+            
+        });
+    });
+    
+    $("#search-bytags a").click(function()
+    {
+        var tags = $("#search-bytags input").val().killWhitespaces();
+        
+        console.log("tags: " + tags);
+        
+        $.getJSON('docs?tags=' + tags, function(data) {
+            
+            console.log("JSON returned:\n" + JSON.stringify(data));
+            
+            data.forEach(function(doc, position)
+            {
+                
+            });
+            
+        });
     });
 
 });
